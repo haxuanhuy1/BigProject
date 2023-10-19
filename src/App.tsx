8600a4c4b05dc5897cb9af0ff13c95ba048eae88
haxuanhuy1 <huyha866@gmail.com>
2023-10-19 11:09:54 +0700

    beta1

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <h1>hà xuân huy </h1>
     </div>
   );
 }
